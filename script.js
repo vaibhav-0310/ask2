8c327e033773df9bdf977e552cf50674515238f1
vaibhav-0310 <vbhargav0310@gmail.com>
2025-04-17 10:59:47 +0530

    name

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const codeInput = document.getElementById('code');
 const submitBtn = document.getElementById('submit-btn');
 const resultDiv = document.getElementById('result');
 
-const secretCode = 'Pankh'; // Replace with your secret code
+const secretCode = 'Shreeyali'; // Replace with your secret code
 const nextPageUrl = 'main.html'; // Replace with the URL of the new page
 
 form.addEventListener('submit', (e) => {
